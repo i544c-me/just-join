27a6d0fcb4535c70864706c3a50f830f492e3653
i544c <vrc@i544c.me>
2024-12-02 22:31:58 +0900

    feat: remove lucida-solid

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,13 +1,12 @@
-import { Hourglass } from 'lucide-solid';
-
 function App() {
   return (
     <>
       <div>
         <h1 class="text-2xl">Just join!</h1>
+        <p>ジョイン可能な状態になると、自動的に自分にインバイトを送信します。</p>
         <div class="stat">
-          <div class="stat-figure text-secondary">
-            <Hourglass />
+          <div class="stat-figure text-2xl">
+            <span>⏳</span>
           </div>
           <div class="stat-title">今の状態</div>
           <div class="stat-value text-primary">待機中...</div>
