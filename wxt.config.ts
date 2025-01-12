92790cba4e169fba67bc26ac72358cc637561c7a
i544c <vrc@i544c.me>
2025-01-12 19:43:55 +0900

    chore: add original icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-solid'],
+  modules: ['@wxt-dev/module-solid', '@wxt-dev/auto-icons'],
   manifest: {
     host_permissions: ['https://vrchat.com/*'],
     permissions: ['cookies', 'storage'],
